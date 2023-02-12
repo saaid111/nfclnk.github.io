addb8de259ca5f6654d9b9a52e5cdc4a35d0540e
saaid111 <93545412+saaid111@users.noreply.github.com>
2023-02-12 20:13:50 +0000

    Add files via upload

---
+++ Untitled-3.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  const searchParams = new URLSearchParams(document.referrer);
+  const serialNumber = searchParams.get('serial');
+  if (serialNumber === "12345") {
+    // Give the user 10 points
+    const points = localStorage.getItem('points') || 0;
+    localStorage.setItem('points', Number(points) + 10);
+    alert("You have earned 10 points! Your total points are: " + (Number(points) + 10));
+  } else {
+    alert("Sorry, you did not come from a valid NFC link.");
+  }
+};
